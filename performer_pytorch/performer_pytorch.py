824a024db93e947eff97508195b658e9a3c549e7
Phil Wang <lucidrains@gmail.com>
2020-10-06 20:04:28 -0700

    finish transcribing the heart of the technique, the orthogonal gaussian kernel

--- performer_pytorch/performer_pytorch.py
+++ performer_pytorch/performer_pytorch.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -10,6 +11,37 @@ def exists(val):
 
 def default(val, d):
     return val if exists(val) else d
+
+# kernel functions
+
+def gaussian_orthogonal_random_matrix(nb_rows, nb_columns, scaling = 0):
+    nb_full_blocks = int(nb_rows / nb_columns)
+
+    block_list = []
+    for _ in range(nb_full_blocks):
+        unstructured_block = torch.randn((nb_columns, nb_columns))
+        q, _ = torch.qr(unstructured_block)
+        q = q.t()
+        block_list.append(q)
+
+    remaining_rows = nb_rows - nb_full_blocks * nb_columns
+    if remaining_rows > 0:
+        unstructured_block = torch.randn((nb_columns, nb_columns))
+        q, _ = torch.qr(unstructured_block)
+        q = q.t()
+        block_list.append(q[:remaining_rows])
+
+    final_matrix = torch.stack(block_list)
+
+    if scaling == 0:
+        multiplier = torch.randn((nb_rows, nb_columns)).norm(dim = 1)
+        print(multiplier.shape)
+    elif scaling == 1:
+        multiplier = math.sqrt((float(nb_columns))) * torch.ones(nb_rows)
+    else:
+        raise ValueError(f'Invalid scaling {scaling}')
+
+    return torch.diag(multiplier) @ final_matrix
 
 # classes
 
