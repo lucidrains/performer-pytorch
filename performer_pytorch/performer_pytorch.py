fix bug with non-cuda linear causal attn

performer_pytorch/performer_pytorch.py
@@ -128,7 +128,7 @@ def causal_linear_attention(q, k, v):
 # inefficient causal linear attention, without cuda code, for reader's reference
 # not being used
 def causal_linear_attention_noncuda(q, k, v):
-    D_inv = torch.einsum('...nd,...nd->...n', q, k.cumsum(dim=-2))
+    D_inv = 1. / torch.einsum('...nd,...nd->...n', q, k.cumsum(dim=-2))
     context = torch.einsum('...nd,...ne->...nde', k, v)
     context = context.cumsum(dim=-3)
     out = torch.einsum('...nde,...nd,...n->...ne', context, q, D_inv)