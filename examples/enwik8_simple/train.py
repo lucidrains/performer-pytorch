update enwik8 example to use automatic mixed precision, and fix a small bug with switching between amp and non-amp

examples/enwik8_simple/train.py
@@ -9,6 +9,7 @@ import torch
 import torch.optim as optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, Dataset
+from torch.cuda.amp import autocast, GradScaler
 
 # constants
 
@@ -46,7 +47,8 @@ model = PerformerLM(
     reversible = True,
     nb_features = 256,
     use_scalenorm = True,
-    local_attn_heads = (8, 8, 8, 6, 4, 2)
+    local_attn_heads = (8, 8, 8, 6, 4, 2),
+    amp_enabled = True
 )
 
 model = AutoregressiveWrapper(model)
@@ -81,6 +83,7 @@ val_loader    = cycle(DataLoader(val_dataset, batch_size = BATCH_SIZE))
 # optimizer
 
 optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+scaler = GradScaler()
 
 # training
 
@@ -88,12 +91,16 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
     model.train()
 
     for __ in range(GRADIENT_ACCUMULATE_EVERY):
-        loss = model(next(train_loader), return_loss = True)
-        loss.backward()
+        with autocast():
+            loss = model(next(train_loader), return_loss = True)
+        scaler.scale(loss).backward()
 
     print(f'training loss: {loss.item()}')
+
+    scaler.unscale_(optim)
     torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-    optim.step()
+    scaler.step(optim)
+    scaler.update()
     optim.zero_grad()
 
     if i % VALIDATE_EVERY == 0:
@@ -102,7 +109,7 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
             loss = model(next(val_loader), return_loss = True)
             print(f'validation loss: {loss.item()}')
 
-    if i % GENERATE_EVERY == 0:
+    if i % GENERATE_EVERY == 0 and i != 0:
         model.eval()
         inp = random.choice(val_dataset)[:-1]
         prime = decode_tokens(inp)

performer_pytorch/performer_pytorch.py
@@ -41,7 +41,7 @@ def softmax_kernel(data, *, projection_matrix, is_query, normalize_data=True, ep
 
     ratio = (projection_matrix.shape[0] ** -0.5)
 
-    projection = repeat(projection_matrix, 'j d -> b h j d', b = b, h = h)
+    projection = repeat(projection_matrix, 'j d -> b h j d', b = b, h = h).type_as(data)
 
     data_dash = torch.einsum('...id,...jd->...ij', (data_normalizer * data), projection)
 
@@ -149,7 +149,7 @@ def causal_linear_attention_noncuda(q, k, v):
     return out
 
 class FastAttention(nn.Module):
-    def __init__(self, dim_heads, nb_features = None, feature_redraw_interval = 0, ortho_scaling = 0, causal = False, generalized_attention = False, kernel_fn = nn.ReLU(), qr_uniform_q = False, amp_enabled = False):
+    def __init__(self, dim_heads, nb_features = None, feature_redraw_interval = 1000, ortho_scaling = 0, causal = False, generalized_attention = False, kernel_fn = nn.ReLU(), qr_uniform_q = False, amp_enabled = False):
         super().__init__()
         nb_features = default(nb_features, int(dim_heads * math.log(dim_heads)))
 
@@ -181,7 +181,7 @@ class FastAttention(nn.Module):
 
         # It's time to redraw the projection matrix
         if exists(self.feature_redraw_interval) and self.calls_since_last_redraw >= self.feature_redraw_interval:
-            self.projection_matrix = self.create_projection(device = device).type_as(q)
+            self.projection_matrix = self.create_projection(device = device)
             self.calls_since_last_redraw = torch.tensor(0)
         # Keep track of how many forward passes we do before we redraw again
         else: